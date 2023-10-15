32e788e91aac6a98555317892e2e2c6f4b469973
JDiaz <jdiaz_007@outlook.com>
2023-10-14 17:44:31 -0700

    added HeadBar (navbar) component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import './App.css';
+import HeadBar from './components/HeadBar'
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <HeadBar />
+
     </div>
   );
 }
